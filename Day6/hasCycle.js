e09537918def5ae3a4d5caa6c851f3e9b43c1824
sir <2362446788@qq.com>
2022-12-12 23:20:59 +0800

    feat: double cursor

--- Day6/hasCycle.js
+++ Day6/hasCycle.js
@@ -1,3 +1,17 @@
+// var hasCycle = function (head) {
+//   let cache = new Set();
+//   while (head) {
+//     if (cache.has(head)) {
+//       return true;
+//     } else {
+//       cache.add(head);
+//       head = head.next;
+//     }
+//   }
+//   return false;
+// };
+
+// 双指针
 var hasCycle = function (head) {
   let cache = new Set();
   while (head) {
