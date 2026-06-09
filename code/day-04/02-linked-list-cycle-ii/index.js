3b74c48520e12a1532059b82df935d0cbb8bb64c
xzz <2362446788@qq.com>
2026-06-09 19:38:15 +0800

    feat: 2026-06-09

--- code/day-04/02-linked-list-cycle-ii/index.js
+++ code/day-04/02-linked-list-cycle-ii/index.js
@@ -1,11 +1,37 @@
 // leetcode：https://leetcode.cn/problems/linked-list-cycle-ii/
 // 代码随想录：https://programmercarl.com/0142.%E7%8E%AF%E5%BD%A2%E9%93%BE%E8%A1%A8II.html
-// 时间复杂度：TODO
-// 空间复杂度：TODO
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
 /**
- * @param {...any} args
- * @return {any}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-var solution = function(...args) {
 
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    // 思路：使用快慢指针，快指针走两步，慢指针走一步，如果有环，一定能相遇，因为快指针相对于慢指针就是多移动一步，有环一定能相遇
+    // 相遇的节点和头结点一直移动，一定能相遇，有可能节点内多移动了很多圈才遇到，如果最后遇到了就返回相遇的那个节点
+    // 否则返回 null
+    let fast = head;
+    let slow = head;
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast === slow) {
+            let cur1 = fast;
+            let cur2 = head;
+            while (cur1 !== cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        }
+    }
+    return null;
 };
--- code/day-04/03-swap-nodes-in-pairs/index.js
+++ code/day-04/03-swap-nodes-in-pairs/index.js
@@ -3,9 +3,51 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var solution = function(...args) {
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    // 使用虚拟头结点
+    let newHead = new ListNode(0, head);
+    // 增加临时引用，不会影响虚拟头结点
+    let cur1 = newHead;
+    // 如果没有两个剩余节点了就不需要再交换
+    while (cur1.next && cur1.next.next) {
+        // 先保存后续还要使用的节点，因为后面调整next之后就没办法找到原来的指向了
+        let temp1 = cur1.next;
+        let temp2 = cur1.next.next;
+        // cur1->1->2->3->4->5->null
+        // temp1=1, temp2=2;
+        // temp1.next=temp2.next，即1->3
+        // temp2.next = temp1，即 2->1
+        // cur1.next = temp2，即cur1->2
+        // 再把cur1节点往后移动两位，现在temp1就是和之前的链表相比相差两位的，cur1 = temp1 和 cur1 = cur1.next.next 是一样的
+        // 最后翻转的链表为 2->1->3->4->5->null，并且 cur1 目前处于 3 的位置，前两位就被交换了
+        temp1.next = temp2.next;
+        temp2.next = temp1;
+        cur1.next = temp2;
+        cur1 = temp1;
+    }
+    return newHead.next;
 
+    // // 递归
+    // // 递归出口
+    // if (head === null || head.next === null) {
+    //     return head;
+    // }
+    // // 将第二个节点 head.next 临时保存下来
+    // let after = head.next;
+    // // 头结点指向下一个节点的下一个节点
+    // head.next = swapPairs(after.next);
+    // // 将第二个节点当做头结点，将其 next 指向头结点
+    // after.next = head;
+    // // 返回新的头结点
+    // return after;
 };
