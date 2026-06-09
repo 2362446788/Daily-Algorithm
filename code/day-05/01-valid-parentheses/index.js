518ccd2e49fa40fc3fc056cd249ac43846a02e39
xzz <2362446788@qq.com>
2026-06-09 20:52:02 +0800

    feat: 2026-06-09

--- code/day-05/01-valid-parentheses/index.js
+++ code/day-05/01-valid-parentheses/index.js
@@ -1,11 +1,42 @@
 // leetcode：https://leetcode.cn/problems/valid-parentheses/
 // 代码随想录：https://programmercarl.com/0020.%E6%9C%89%E6%95%88%E7%9A%84%E6%8B%AC%E5%8F%B7.html
-// 时间复杂度：TODO
-// 空间复杂度：TODO
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
 /**
- * @param {...any} args
- * @return {any}
+ * @param {string} s
+ * @return {boolean}
  */
-var solution = function(...args) {
-
+var isValid = function(s) {
+    const mapping = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+    }
+    let stack = [];
+    for (const str of s) {
+        if (str in mapping) {
+            stack.push(mapping[str]);
+            continue;
+        }
+        // 1
+        if (str !== stack[stack.length - 1]) {
+            return false;
+        }
+        // 2
+        if (stack.length === 0) {
+            return false;
+        }
+        // 3
+        stack.pop();
+        // 简化写法：因为空数组 pop 也没问题，所以直接将栈里面的数据取出来进行比较，如果不相等就返回 false
+        // 这个写法满足了上面三个执行逻辑 1 2 3
+        // 其中 2 因为如果是空栈的话 pop 取到的是 undefined，和字符串也不匹配，也走到 false
+        // if (str !== stack.pop()) {
+        //     return false;
+        // }
+    }
+    if (stack.length > 0) {
+        return false;
+    }
+    return true;
 };
--- code/day-05/02-reverse-string/index.js
+++ code/day-05/02-reverse-string/index.js
@@ -1,11 +1,16 @@
 // leetcode：https://leetcode.cn/problems/reverse-string/
 // 代码随想录：https://programmercarl.com/0344.%E5%8F%8D%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2.html
-// 时间复杂度：TODO
-// 空间复杂度：TODO
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
 /**
- * @param {...any} args
- * @return {any}
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
  */
-var solution = function(...args) {
-
+var reverseString = function(s) {
+    // 使用双指针，头尾指针往中间移动，然后交换位置，循环结束位置为 i < j，因为 i = j 没有意义，同一个值不需要交换
+    for(let i = 0, j = s.length - 1; i < j; i++, j--) {
+        let temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
 };
