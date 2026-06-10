8e933594058cb4e40a4b0e2b67e658f237b197c8
wang_tao <2362446788@qq.com>
2026-06-10 18:32:53 +0800

    feat: 2026-06-10

--- code/day-05/03-reverse-words-in-a-string/index.js
+++ code/day-05/03-reverse-words-in-a-string/index.js
@@ -1,11 +1,82 @@
 // leetcode：https://leetcode.cn/problems/reverse-words-in-a-string/
 // 代码随想录：https://programmercarl.com/0151.%E7%BF%BB%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2%E9%87%8C%E7%9A%84%E5%8D%95%E8%AF%8D.html
-// 时间复杂度：TODO
-// 空间复杂度：TODO
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
 /**
- * @param {...any} args
- * @return {any}
+ * @param {string} s
+ * @return {string}
  */
-var solution = function(...args) {
-
+var reverseWords = function (s) {
+  let arr = Array.from(s);
+  // 先移除无效的空格
+  let newArr = removeSpace(arr);
+  // 先整体反转字符串
+  reverse(arr, 0, arr.length - 1);
+  // 获取每一个单词进行反转
+  // 记录单词的开始下标 start
+  let start = 0;
+  // 记录单词结束下标 i
+  for (let i = 0; i <= arr.length; i++) {
+    // 找到单词中的空字符串或者遍历到了数组结尾，都需要进行这个单词反转
+    if (arr[i] === " " || i === arr.length) {
+      reverse(arr, start, i - 1);
+      // 反转之后start指向剩下需要反转的内容
+      start = i + 1;
+    }
+  }
+  return arr.join("");
 };
+// function removeSpace(arr) {
+//     let slow = 0;
+//     let fast = 0;
+//     while (fast < arr.length) {
+//         if (arr[fast] === ' ' && (arr[fast-1] === ' ' || fast === 0)) {
+//             fast++;
+//         } else {
+//             arr[slow] = arr[fast];
+//             fast++;
+//             slow++;
+//         }
+//     }
+//     // '  he  wo  '
+//     // 移除末尾空格
+//     // 因为 slow 目前循环之后就是数组的长度，指向数组下一个要写入的位置，所以 slow - 1才是才是最后一个数组的下标
+//     arr.length = arr[slow - 1] === ' ' ? slow - 1 : slow;
+//     return arr;
+// }
+function removeSpace(arr) {
+  let slow = 0;
+  let fast = 0;
+  while (fast < arr.length) {
+    // 如果不是空字符串，那么就把fast的值赋值给slow，两者同时加一
+    if (arr[fast] !== " ") {
+      // 这里还需要保留单子中的一个空字符串，只需要单词中的增加一个
+      if (slow !== 0) {
+        arr[slow] = " ";
+        slow++;
+      }
+      // 然后把所有连续的非空字符串的字符都赋值给slow，两者同时加一，往右移动
+      while (fast < arr.length && arr[fast] !== " ") {
+        arr[slow] = arr[fast];
+        fast++;
+        slow++;
+      }
+    } else {
+      fast++;
+    }
+  }
+  // 最后数组长度就是 slow 的长度，因为循环完之后slow是指向下一个要写入的位置，但是fast遍历完了，就没有要写入的
+  arr.length = slow;
+  return arr;
+}
+// 翻转从 start 到 end 的字符
+function reverse(strArr, start, end) {
+  let left = start;
+  let right = end;
+  while (left < right) {
+    // 交换
+    [strArr[left], strArr[right]] = [strArr[right], strArr[left]];
+    left++;
+    right--;
+  }
+}
--- code/day-17/01-reverse-string-ii/index.js
+++ code/day-17/01-reverse-string-ii/index.js
@@ -1,11 +1,70 @@
 // leetcode：https://leetcode.cn/problems/reverse-string-ii/
 // 代码随想录：https://programmercarl.com/0541.%E5%8F%8D%E8%BD%AC%E5%AD%97%E7%AC%A6%E4%B8%B2II.html
-// 时间复杂度：TODO
-// 空间复杂度：TODO
-/**
- * @param {...any} args
- * @return {any}
- */
-var solution = function(...args) {
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+// 双指针写法
+var reverseStr = function (s, k) {
+  // js 中没办法对字符串进行翻转，需要借用数组
+  let arr = s.split("");
+  // 题目要求每 2k 个字符，那么就可以每次遍历 2k 个字符
+  for (let i = 0; i < arr.length; i += 2 * k) {
+    // 每次遍历了 2k 个字符，需要将前 k 个反转
+    // 那么就使用双指针来反转
+    let left = i;
+    // 为什么是 i + k - 1 呢？
+    // ['a', 'b', 'c'], i = 0, k = 3，此时要反转这三个，如果直接使用 i + k = 3，超过数组下标了，越界了，执行结果就不正确，因此需要判断是否越界，越界了 就需要只能访问数组最后一项
+    let right = i + k - 1 > arr.length ? arr.length : i + k - 1;
+    // 使用双指针移动交换位置进行反转
+    while (left < right) {
+      let temp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = temp;
+      left++;
+      right--;
+    }
+  }
+  // 最后输出字符串
+  return arr.join("");
+};
 
+// 条件判断写法
+var reverseArr = function (arr, i, k) {
+  let arr1 = arr.slice(0, i);
+  let arr2 = arr.slice(i, k);
+  let arr3 = arr.slice(k, arr.length);
+  let left = 0;
+  let right = arr2.length - 1;
+  while (left < right) {
+    let temp = arr2[right];
+    arr2[right] = arr2[left];
+    arr2[left] = temp;
+    left++;
+    right--;
+  }
+  // 这样写更方便
+  // return [...arr.slice(0, i), ...arr2, ...arr.slice(k, arr.length)];
+  return arr1.concat(arr2).concat(arr3);
+};
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+  // js 中没办法对字符串进行翻转，需要借用数组
+  let arr = s.split("");
+  // 题目要求每 2k 个字符，那么就可以每次遍历 2k 个字符
+  for (let i = 0; i < arr.length; i += 2 * k) {
+    // 每次遍历了 2k 个字符，需要将前 k 个反转
+    // 取到的字符可能有 2k 个也可能小于 2k 个，但反转不关心是否有 2k 个字符
+    // 只关心是否满足 k 个字符
+    // 1. 如果前 k 个字符都存在，即 i + k <= arr.length，那么反转前 k 个字符
+    if (i + k <= arr.length) {
+      arr = reverseArr(arr, i, i + k);
+    } else {
+      // 2. 如果不满足 k 个，那么就将剩下的字符都反转
+      arr = reverseArr(arr, i, arr.length);
+    }
+  }
+  return arr.join("");
 };
