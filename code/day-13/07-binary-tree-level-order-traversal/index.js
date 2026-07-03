e0d49f11e0b14e0c2b8c54a63ca8d6098d96c3ad
sir. <2362446788@qq.com>
2026-07-03 11:02:04 +0800

    feat: levelOrder

--- code/day-13/07-binary-tree-level-order-traversal/index.js
+++ code/day-13/07-binary-tree-level-order-traversal/index.js
@@ -3,9 +3,44 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {TreeNode} root
+ * @return {number[][]}
  */
-var solution = function(...args) {
-
+var levelOrder = function (root) {
+  // 思路分析
+  // 二叉树的结构没办法直接做层序遍历，需要借助队列来实现
+  // 把头节点放入队列中，依次循环队列元素，并且每一次记录需要弹出多少个元素，在遍历的过程中将元素弹出，并且把其左右子树加入队列中，这样队列的值就会一直不为空，能接着循环执行，直到把所有节点都弹出去结束
+  // 定义结果
+  if (root === null) {
+    return [];
+  }
+  let result = [];
+  // 队列，将头节点放入队列中
+  let queue = [root];
+  // 循环队列
+  while (queue.length) {
+    // 存放弹出的节点
+    let value = [];
+    // 记录需要弹出多少个元素
+    // 不能直接 while (queue.length)，因为队列的数据一直在变化
+    let num = queue.length;
+    while (num) {
+      // 把队列队头数据弹出
+      let item = queue.shift();
+      // 加入结果中
+      value.push(item.val);
+      // 将 num 减一
+      num--;
+      // 把左右子树加入队列中
+      if (item.left) {
+        queue.push(item.left);
+      }
+      if (item.right) {
+        queue.push(item.right);
+      }
+    }
+    // 把弹出的结果加入最终结果中
+    result.push(value);
+  }
+  return result;
 };
