e252566d521715934e67286a785a0a0e21602b43
sir. <2362446788@qq.com>
2026-07-05 09:52:11 +0800

    feat: mergeTwoLists

--- code/day-17/03-merge-two-sorted-lists/index.js
+++ code/day-17/03-merge-two-sorted-lists/index.js
@@ -3,9 +3,29 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
  */
-var solution = function(...args) {
-
+var mergeTwoLists = function (list1, list2) {
+  // 构造一个新链表
+  // 移动两个链表，全部移动到最后才结束
+  let newHead = new ListNode();
+  // 复制一个新链表的引用，方便构造新的链表
+  let pre = newHead;
+  // 将两个链表遍历
+  // 取出最小的那个值给到 pre.next，并且移动对应的链表，并且将 pre 也往后移动
+  while (list1 !== null && list2 !== null) {
+    if (list1.val <= list2.val) {
+      pre.next = list1;
+      list1 = list1.next;
+    } else {
+      pre.next = list2;
+      list2 = list2.next;
+    }
+    pre = pre.next;
+  }
+  // 遍历完以后，最多只有一个还没有被完全合并完，直接将链表末尾指向未合并完的链表即可
+  pre.next = list1 === null ? list2 : list1;
+  return newHead.next;
 };
