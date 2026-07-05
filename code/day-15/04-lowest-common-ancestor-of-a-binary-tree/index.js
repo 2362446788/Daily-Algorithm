19572b1076794c9b13ef616add3750aae41a4bac
sir. <2362446788@qq.com>
2026-07-05 08:34:18 +0800

    feat: lowestCommonAncestor

--- code/day-15/04-lowest-common-ancestor-of-a-binary-tree/index.js
+++ code/day-15/04-lowest-common-ancestor-of-a-binary-tree/index.js
@@ -3,9 +3,46 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
  */
-var solution = function(...args) {
-
+// 思路
+// 使用递归回溯的方法处理
+// 递归三部曲
+// 1. 入参和返回值
+//    root, p, q
+// 2. 递归出口
+//    root === null 返回 root
+//    root === p || root === q 返回 root
+// 3. 单次递归的逻辑
+//    后序遍历 左右中，分别计算左右子树的结果再来判断
+//    - 左右都不为空，那么当前 root 就是最近公共祖先
+//    - 左为空右不为空返回右节点
+//    - 左不为空右为空返回左节点
+//    - 都为空返回 null
+var lowestCommonAncestor = function (root, p, q) {
+  function recursive(root, p, q) {
+    if (root === null) {
+      return root;
+    }
+    if (root === p || root === q) {
+      return root;
+    }
+    // 后序遍历 左右中
+    let leftNode = recursive(root.left, p, q);
+    let rightNode = recursive(root.right, p, q);
+    // 中的处理
+    if (leftNode !== null && rightNode !== null) {
+      return root;
+    } else if (leftNode === null && rightNode !== null) {
+      return rightNode;
+    } else if (leftNode !== null && rightNode === null) {
+      return leftNode;
+    } else {
+      return null;
+    }
+  }
+  return recursive(root, p, q);
 };
