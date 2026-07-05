22cca235f633a2b17d58c8575a7cc6932e080895
sir. <2362446788@qq.com>
2026-07-05 09:51:43 +0800

    feat: hasCycle

--- code/day-17/01-linked-list-cycle/index.js
+++ code/day-17/01-linked-list-cycle/index.js
@@ -3,9 +3,21 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {ListNode} head
+ * @return {boolean}
  */
-var solution = function(...args) {
-
+// 思路
+// 使用快慢指针去走，如果有环，那么快慢指针一定会在环中相遇
+// 遍历终止条件：快指针为空或者快指针的next为空
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
 };
