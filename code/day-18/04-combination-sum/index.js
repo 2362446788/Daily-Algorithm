e94e41783bf0ca555a7f5e5dd491f49d01cd2c55
sir. <2362446788@qq.com>
2026-07-06 10:42:01 +0800

    feat: combinationSum

--- code/day-18/04-combination-sum/index.js
+++ code/day-18/04-combination-sum/index.js
@@ -3,9 +3,42 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
  */
-var solution = function(...args) {
-
+// 使用组合的模版
+var combinationSum = function (candidates, target) {
+  let result = [];
+  let path = [];
+  // 剪枝：使用排序之后然后在 for 循环增加判断，如果当前值加上 sum 已经大于 target，那么后续也不需要再递归下去了
+  // 剪枝@1 升序排序数组
+  candidates = candidates.sort((a, b) => a - b);
+  // 增加 sum 来计算是否结果相等
+  function backtracking(candidates, target, sum, startIndex) {
+    if (sum > target) {
+      return;
+    }
+    if (sum === target) {
+      result.push(path.slice());
+      return;
+    }
+    // 剪枝@2 i < candidates.length && sum + candidates[i] <= target
+    // 不剪枝：i < candidates.length
+    for (
+      let i = startIndex;
+      i < candidates.length && sum + candidates[i] <= target;
+      i++
+    ) {
+      path.push(candidates[i]);
+      sum += candidates[i];
+      // 因为单次递归下去可以重复查找数据，所以 startIndex 使用 i 而不是 i + 1
+      backtracking(candidates, target, sum, i);
+      // 回溯操作
+      path.pop();
+      sum -= candidates[i];
+    }
+  }
+  backtracking(candidates, target, 0, 0);
+  return result;
 };
