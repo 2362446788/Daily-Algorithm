a93e5aa630036601c06d699526066fa55b73e308
sir. <2362446788@qq.com>
2026-07-06 10:41:15 +0800

    feat: combine

--- code/day-18/01-combinations/index.js
+++ code/day-18/01-combinations/index.js
@@ -3,9 +3,41 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
  */
-var solution = function(...args) {
-
+var combine = function (n, k) {
+  // 回溯算法的三部曲
+  // 1. 递归函数参数和返回值
+  // 2. 递归出口
+  // 3. 单层遍历执行的操作
+  let result = [];
+  let paths = [];
+  // startIndex 是让每一次递归从下一个数开始查找，因为这是组合问题，不是排列问题 [1, 2] 和 [2, 1] 是一样的
+  function backtracking(n, k, startIndex) {
+    if (paths.length === k) {
+      result.push(paths.slice());
+      return;
+    }
+    // 循环本层集合元素
+    // for (let i = startIndex; i <= n; i++) {
+    //     paths.push(i);
+    //     backtracking(n, k, i + 1);
+    //     // 回溯操作
+    //     paths.pop();
+    // }
+    // 剪枝操作
+    // i <= n - (k - paths.length) + 1
+    // k - paths.length 表示还需要多少个数
+    // n - (k - paths.length) + 1 表示至多可以遍历到这里，后续的遍历都没办法获取到符合条件的数据
+    for (let i = startIndex; i <= n - (k - paths.length) + 1; i++) {
+      paths.push(i);
+      backtracking(n, k, i + 1);
+      // 回溯操作
+      paths.pop();
+    }
+  }
+  backtracking(n, k, 1);
+  return result;
 };
