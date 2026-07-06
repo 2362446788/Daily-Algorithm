553fc2b8d5557b3348d274f409adf41e84c42d72
sir. <2362446788@qq.com>
2026-07-06 10:41:31 +0800

    feat: permute

--- code/day-18/02-permutations/index.js
+++ code/day-18/02-permutations/index.js
@@ -3,9 +3,33 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {number[]} nums
+ * @return {number[][]}
  */
-var solution = function(...args) {
-
+var permute = function (nums) {
+  let result = [];
+  let path = [];
+  // 使用一个used数组来标识哪个值被使用了（树枝节点）
+  function backtracking(nums, used) {
+    // 收集的长度等于原始数组的长度就说明收集好了，需要加入到 result 中
+    if (path.length === nums.length) {
+      // 因为 path 是一个引用地址，需要将其复制出来，不然结果中使用的都是同一个引用地址
+      result.push(path.slice());
+      return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+      // 判断是否被使用过了
+      if (used[i]) {
+        continue;
+      }
+      used[i] = true;
+      path.push(nums[i]);
+      backtracking(nums, used);
+      // 回溯
+      used[i] = false;
+      path.pop();
+    }
+  }
+  backtracking(nums, []);
+  return result;
 };
