c95f34357fc2bd223d0f5e518cda6e2463bf6cb1
sir. <2362446788@qq.com>
2026-07-06 10:41:49 +0800

    feat: subsets

--- code/day-18/03-subsets/index.js
+++ code/day-18/03-subsets/index.js
@@ -3,9 +3,46 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {number[]} nums
+ * @return {number[][]}
  */
-var solution = function(...args) {
+var subsets = function (nums) {
+  let result = [];
+  let path = [];
+  function backtracking(nums, startIndex) {
+    result.push(path.slice());
+    for (let i = startIndex; i < nums.length; i++) {
+      path.push(nums[i]);
+      backtracking(nums, i + 1);
+      path.pop();
+    }
+  }
+  backtracking(nums, 0);
+  return result;
+};
 
+// 子集II
+// leetcode：https://leetcode.cn/problems/subsets-ii/
+// 代码随想录：https://programmercarl.com/0090.%E5%AD%90%E9%9B%86II.html
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  let result = [];
+  let path = [];
+  nums = nums.sort((a, b) => a - b);
+  function backtracking(nums, startIndex) {
+    result.push(path.slice());
+    for (let i = startIndex; i < nums.length; i++) {
+      if (i > startIndex && nums[i] === nums[i - 1]) {
+        continue;
+      }
+      path.push(nums[i]);
+      backtracking(nums, i + 1);
+      path.pop();
+    }
+  }
+  backtracking(nums, 0);
+  return result;
 };
