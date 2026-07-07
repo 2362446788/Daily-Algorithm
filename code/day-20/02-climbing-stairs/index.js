099bc7ca67b8c52f1066033d74abccab65b1ddbb
sir. <2362446788@qq.com>
2026-07-07 11:47:14 +0800

    feat: climbStairs

--- code/day-20/02-climbing-stairs/index.js
+++ code/day-20/02-climbing-stairs/index.js
@@ -3,9 +3,24 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {number} n
+ * @return {number}
  */
-var solution = function(...args) {
-
+var climbStairs = function (n) {
+  // 第1阶：1种 1
+  // 第2阶：2种 1 + 1 和 2
+  // 第3阶：第2阶+第1阶 = 3，可以从第1阶爬2个台阶到达，也可以从第2阶爬1个台阶到达，即有第1阶+第2阶的方式到达
+  // 第4阶：第3阶+第2阶
+  // 第i阶：第i-1阶+第i-2阶
+  // 1. dp数组及下标的定义：dp[i]表示第i阶有dp[i]中方式
+  // 2. 递推公式：dp[i] = dp[i-1]+dp[i-2]
+  // 3. dp数组初始化：n >=1 所以从 dp[1] = 1，dp[2] = 2
+  // 4. 遍历顺序：从左到右，从i=3开始
+  let dp = [];
+  dp[1] = 1;
+  dp[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
 };
