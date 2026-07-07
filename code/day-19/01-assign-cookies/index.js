d74e4b9c5ca7e4a674a313fb088919565aee0f11
sir. <2362446788@qq.com>
2026-07-07 11:46:16 +0800

    feat: findContentChildren

--- code/day-19/01-assign-cookies/index.js
+++ code/day-19/01-assign-cookies/index.js
@@ -3,9 +3,25 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
  */
-var solution = function(...args) {
-
+// 思路
+// 使用贪心算法，用大饼干满足大胃口的孩子，这样就能更多的满足孩子的胃口
+var findContentChildren = function (g, s) {
+  g = g.sort((a, b) => a - b);
+  s = s.sort((a, b) => a - b);
+  // 记录满足了几个孩子
+  let result = 0;
+  // 记录饼干移动的下标
+  let index = s.length - 1;
+  for (let i = g.length - 1; i >= 0; i--) {
+    // 如果有大饼干能满足孩子就往前移动
+    if (index >= 0 && s[index] >= g[i]) {
+      index--;
+      result++;
+    }
+  }
+  return result;
 };
