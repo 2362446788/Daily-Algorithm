0e2653ef8a954e4cd9075071819dac72cd542936
sir. <2362446788@qq.com>
2026-07-07 11:46:36 +0800

    feat: canJump

--- code/day-19/03-jump-game/index.js
+++ code/day-19/03-jump-game/index.js
@@ -3,9 +3,20 @@
 // 时间复杂度：TODO
 // 空间复杂度：TODO
 /**
- * @param {...any} args
- * @return {any}
+ * @param {number[]} nums
+ * @return {boolean}
  */
-var solution = function(...args) {
-
+// 维护一个可跳跃的最大范围 cover（是数组下标），每一次遍历都增加这个范围，只要找到一次 cover 大于等于数组最后的下标就直接返回 true，否则最后返回 false
+var canJump = function (nums) {
+  let cover = 0;
+  // 只在 cover 的范围进行循环
+  for (let i = 0; i <= cover; i++) {
+    // 更新 cover，i + nums[i] 就是能覆盖的最大下标
+    cover = Math.max(cover, i + nums[i]);
+    // 如果 cover 大于等于 nums.length - 1 直接返回 true
+    if (cover >= nums.length - 1) {
+      return true;
+    }
+  }
+  return false;
 };
